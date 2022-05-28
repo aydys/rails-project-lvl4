d2c304df83aada442936cab99768664d4b4383fb
Aidys Dongak <aidongak23@gmail.com>
2022-05-28 10:29:52 +0700

    create auth concern

---
+++ app/controllers/concerns/auth_concern.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module AuthConcern
+  extend ActiveSupport::Concern
+
+  included do
+    def sign_in(user)
+      session[:user_id] = user.id
+    end
+
+    def sing_out
+      reset_session
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def signed_in?
+      current_user.present?
+    end
+
+    def authenticate_user
+      return if signed_in?
+
+      flash[:alert] = t('fail_autentication')
+      redirect_back(fallback_location: root_url)
+    end
+  end
+end
