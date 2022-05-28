64d2a503f674e8ae711235160f4041e9ae4f8f83
Aidys Dongak <aidongak23@gmail.com>
2022-05-28 10:32:28 +0700

    refactored home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope module: :web do
+    root 'home#index'
+    
+    post 'auth/:provider', to: 'auth#request', as: :auth_request
+    get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
+
+    resource :session, only: :destroy
+  end
 end
